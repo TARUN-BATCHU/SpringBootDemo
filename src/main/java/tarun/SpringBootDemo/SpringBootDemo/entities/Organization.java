532622d369fd3b80efa6fe96983a9907a70db46d
BATCHU-TARUN <tarunbatchu2000@gmail.com>
2022-10-06 12:59:02 +0530

    tried with different JPA mapping annotiations

--- src/main/java/tarun/SpringBootDemo/SpringBootDemo/entities/Organization.java
+++ src/main/java/tarun/SpringBootDemo/SpringBootDemo/entities/Organization.java
@@ -16,8 +16,21 @@ public @Data class Organization {
 
     private String organizationName;
 
+    // one organization has many students (user is a student)
     @OneToMany(mappedBy = "organization",cascade = CascadeType.ALL,orphanRemoval = true)
     @JsonIgnore
     private List<User> user;
+
+    // one organization will have one state head
+    //@OneToOne
+    //private User user;
+
+    // many students visit many organizations before joining
+    //@ManyToMany
+    //private List<User> user;
+
+    //one principle for one school
+    //@OneToOne
+    //private User user;
 
 }
--- src/main/java/tarun/SpringBootDemo/SpringBootDemo/entities/User.java
+++ src/main/java/tarun/SpringBootDemo/SpringBootDemo/entities/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity(name = "User_Details")
 @NoArgsConstructor
@@ -18,8 +19,19 @@ public @Data class User {
 
     private LocalDate dateOfBirth;
 
-    @JsonIgnore
+    // one student will study in one organization (user is a student)
     @OneToOne
     private Organization organization;
 
+    // state organization head will take of all organizations in that state
+    //@OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
+    //private List<Organization> organization;
+
+    // many students vist organization expo
+    //@ManyToMany(mappedBy = "user",cascade = CascadeType.ALL)
+    //private List<Organization> organization;
+
+    //one principle for one school
+    //@OneToOne
+    //private Organization organization;
 }
